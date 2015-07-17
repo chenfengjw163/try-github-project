73430617b059f4089210f6e59a9df39c203e36fa
chenfengjw163 <348018533@qq.com>
2015-07-17 14:22:25 +0800

    Update AccountController.cs

--- WebApplication2/Controllers/AccountController.cs
+++ WebApplication2/Controllers/AccountController.cs
@@ -28,7 +28,7 @@ namespace WebApplication2.Controllers
 
         public AccountController()
         {
-            Console.WriteLine("这是我的修改");
+            Console.WriteLine("这是我的修改2222222222222222222");
         }
 
         public AccountController(ApplicationUserManager userManager,
